59e4590b93a65f289b17e8615772e655089fb812
Wenhan Zhang <erikzhangwh@gmail.com>
2018-10-28 21:18:31 -0400

    V7

---
+++ src/H1BTopKSelector.java
@@ -0,0 +1,159 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class H1BTopKSelector {
+
+	public static void main(String[] args) {
+		String inputFilePath = args[0];
+		String outputOccupPath = args[1];
+		String outputStatePath = args[2];
+		H1BTopKSelector selector = new H1BTopKSelector();
+		selector.selectTopK(inputFilePath, outputStatePath, outputOccupPath, 10);
+	}
+	
+	public boolean selectTopK(String inputFilePath, String outputStatePath, String outputOccupPath, int numK) {
+		//Build two maps to store (state, count) and (job, count) pairs
+		Map<String, Integer> stateMap = new HashMap<>();
+		Map<String, Integer> jobMap = new HashMap<>();
+		int certifiedNum = createMapsFromRawData(inputFilePath, stateMap, jobMap);
+		if (certifiedNum == 0) {
+			return false;
+		}
+		//Get lists stored the sorted top K keys in the hash maps
+		List<String> topKstateKeyList = TopKSelector.getTopKKeysFromMap(stateMap,numK);
+		List<String> topKjobKeyList = TopKSelector.getTopKKeysFromMap(jobMap,numK);
+		
+		//Compute the percentage of top K keys in the whole certified cases
+		List<String> topKstatePercent = computePercent(topKstateKeyList, stateMap, certifiedNum);
+		List<String> topKjobPercent = computePercent(topKjobKeyList, jobMap, certifiedNum);
+		
+		//Save the information of top K entries as local text files
+		saveAsText(topKjobKeyList, topKjobPercent, jobMap, outputOccupPath, "job");
+		saveAsText(topKstateKeyList, topKstatePercent, stateMap, outputStatePath,"state");
+		return true;
+	}
+	
+	public int createMapsFromRawData(String inputFilePath, Map<String, Integer> stateMap, Map<String, Integer> jobMap) {
+		int certifiedNum = 0;
+		try {
+			
+			//read contents from input.csv
+			BufferedReader bfreader = new BufferedReader(new FileReader(inputFilePath));
+			String header = bfreader.readLine();
+			
+			//get target column indexes from header line
+			//targetCols = [column of status, column of work state, column of occupation name]
+			int[] targetCols = getTargetColIndices(header, new String[]{".*STATUS.*", ".*SOC_NAME.*", ".*WORK.*STATE.*"});
+			System.out.println(targetCols[0] + " " + targetCols[1] +  " " + targetCols[2]);
+			//read and process data case by case
+			String line = "";
+			while((line = bfreader.readLine()) != null) {
+				//prepocessEntry() unifies the format of each entry by eliminating some edge cases.
+				String[] fields = preprocessEntry(line).split(";");
+				//If the case is not certified, ignore it.
+				if (!fields[targetCols[0]].toUpperCase().equals("CERTIFIED")) {
+					continue;
+				}
+				//extract work state and occupation name
+				String job = fields[targetCols[1]];
+				String state = fields[targetCols[2]];
+				
+				//update counts and number of certified cases
+				certifiedNum++;
+				stateMap.put(state, stateMap.getOrDefault(state, 0) + 1);
+				jobMap.put(job,jobMap.getOrDefault(job, 0) + 1);
+			}
+			bfreader.close();
+			System.out.print("Total number of different certified states is " + stateMap.size() + "; of jobs is: " + jobMap.size() + "\n");
+			return certifiedNum;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return certifiedNum;
+	}
+	
+	// search the column indices of the corresponding keywords
+	public int[] getTargetColIndices(String header, String[] keyPatterns) {
+		if (header == null) {
+			System.out.println("Empty header!");
+			return null;
+		}
+		int[] targetColumns = new int[keyPatterns.length];
+		for (int i = 0; i < targetColumns.length; i++) {
+			targetColumns[i] = -1;
+		}
+		String[] atrributes = header.toUpperCase().split(";");
+		for (int i = 0; i < atrributes.length; i++) {
+			for(int j = 0; j < keyPatterns.length; j++) {
+				if (atrributes[i].matches(keyPatterns[j]) && targetColumns[j] == -1) {
+					targetColumns[j] = i;
+				}
+			}
+		}
+		return targetColumns;
+	}
+	
+	public String preprocessEntry(String line) {
+		char[] charArr = line.toUpperCase().toCharArray();
+		int quoteNum = 0;
+		for (int i = 0; i < charArr.length; i++) {
+			if (charArr[i] == ';' && quoteNum % 2 == 1) {
+				charArr[i] = 'a';
+			}
+			if (charArr[i] == '\"') {
+				quoteNum++;
+			}
+		}
+		return new String(charArr);
+	}
+	
+	public List<String> computePercent(List<String> keyList, Map<String, Integer> map, int totalNum){
+		List<String> result = new ArrayList<>();
+		if (totalNum == 0) {
+			return result;
+		}
+		for (String key : keyList) {
+			Double percent = (double)map.get(key)/totalNum * 100;
+			//rounded off to 1 decimal place.
+			result.add(String.format("%.1f", percent));
+		}
+		return result;
+	}
+	
+	public boolean saveAsText(List<String> topKKeyList, List<String> percentList, Map<String, Integer> map, String filePath, String sortField) {
+		try {
+			//delete old text file first.
+			File f1 = new File(filePath);
+			if (f1.isFile()) {
+				f1.delete();
+			}
+			//write new data into the file.
+			BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
+			if (sortField.contains("job")) {
+				writer.append("TOP_OCCUPATIONS;NUMBER_CERTIFIED_APPLICATIONS;PERCENTAGE");
+			} else if (sortField.contains("state")) {
+				writer.append("TOP_STATES;NUMBER_CERTIFIED_APPLICATIONS;PERCENTAGE");
+			}
+			for (int i = 0; i < topKKeyList.size(); i++) {
+				writer.newLine();
+				writer.append(topKKeyList.get(i) + ";" + map.get(topKKeyList.get(i)) + ";" + percentList.get(i) + "%");
+			}
+			writer.close();
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+}
--- src/TopKSelector.java
+++ src/TopKSelector.java
@@ -1,193 +1,62 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-
 import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 
-public class TopKSelector {
 
-	public static void main(String[] args) {
-		String inputFilePath = args[0];
-		String outputOccupPath = args[1];
-		String outputStatePath = args[2];
-		TopKSelector selector = new TopKSelector();
-		selector.selectTopK(inputFilePath, outputStatePath, outputOccupPath, 10);
-	}
-	
-	public boolean selectTopK(String inputFilePath, String outputStatePath, String outputOccupPath, int numK)	{
-		try {
-			//read contents from input.csv
-			BufferedReader bfreader = new BufferedReader(new FileReader(inputFilePath));
-			String header = bfreader.readLine();
-			if (header == null) { //empty file
-				bfreader.close();
-				System.out.println("Input file is empty");
-				return false;
+/* This class sorted (key, value) pairs stored in a map and return the top k of them with higher priority as a list.
+ * Input: map + other necessary parameter
+ * Output: sorted list of top k elements selected in the map
+ * Each method of the class implements one selecting and sorting algorithm
+ * Function 1: minHeap (priority queue)
+ * Function 2: bucket sort
+ */
+public class TopKSelector {	
+	//Function 1: Use minHeap to select top K
+	public static List<String> getTopKKeysFromMap(Map<String, Integer> hashMap, int k) {
+		PriorityQueue<Map.Entry<String, Integer>> topKPq = new PriorityQueue<>((a, b) -> {
+			if (a.getValue() != b.getValue()) {
+				return a.getValue() - b.getValue();
+			} else {
+				return -a.getKey().compareToIgnoreCase(b.getKey());
 			}
-			int t1 = (int) System.currentTimeMillis();
-			//Analyze the locations of status, work state and occupation name in the entry
-			String[] attributes = header.split(";");
-			int StatusCol = -1;
-			int WorkStateCol = -1;
-			int SOCnameCol = -1;
-			for (int i = 0; i < attributes.length; i++) {
-				String attr = attributes[i].toLowerCase();
-				if (attr.contains("status") && StatusCol == -1) {
-					StatusCol = i;
-				}
-				if (attr.contains("soc_name") && SOCnameCol == -1) {
-					SOCnameCol = i;
-				}
-				if (attr.contains("work") && attr.contains("state") && WorkStateCol == -1) {
-					WorkStateCol = i;
-				}
-			}
-			
-			int certifiedNum = 0;
-			//Use hash tables to store certified states and jobs and their counts
-			Map<String, Integer> states = new HashMap<>();
-			Map<String, Integer> jobs = new HashMap<>();
-			
-			String line = new String();
-			//read cases line by line
-			while((line = bfreader.readLine()) != null) {
-				//prepocessEntry() unifies the format of each entry by eliminating some edge cases.
-				String goodLine = preprocessEntry(line);
-				String[] attri = goodLine.split(";");
-				String status = attri[StatusCol];
-				if (!status.toUpperCase().equals("CERTIFIED")) {
-					continue;
-				}
-				
-				//extract work state and occupation name
-				String state = attri[WorkStateCol];
-				String job = attri[SOCnameCol];
-				
-				//update counts and number of certified cases
-				certifiedNum++;
-				states.put(state, states.getOrDefault(state, 0) + 1);
-				jobs.put(job,jobs.getOrDefault(job, 0) + 1);
-			}
-			
-			int t2 = (int) System.currentTimeMillis();
-			System.out.print("Time to read file and establish hash tables: " + (t2 - t1) + "\n");
-			System.out.print("Total number of different certified states is " + states.size() + "; of jobs is: " + jobs.size() + "\n");
-			
-			//Get top k entries from prioprity queue
-			List<Map.Entry<String, Integer>> topKstatesList = getTopKFromMap(states,numK);
-			List<String> statePercent = new ArrayList<>();
-			//calculate percentage
-			for (Map.Entry<String, Integer> entry : topKstatesList) {
-				double percent = (double) entry.getValue()/ certifiedNum * 100;
-				statePercent.add(String.format("%.1f", percent));
-			}
-			
-			List<Map.Entry<String, Integer>> topTKtitlesList = getTopKFromMap(jobs,numK);
-			List<String> titlePercent = new ArrayList<>();
-			for (Map.Entry<String, Integer> entry : topTKtitlesList) {
-				double percent = (double) entry.getValue()/ certifiedNum * 100;
-				titlePercent.add(String.format("%.1f", percent));
-			}
-			int t3 = (int) System.currentTimeMillis();
-			System.out.print("Time of top K in using pq: " + (t3 - t2) + "\n");
-			
-			//save results as output files
-			saveAsText(topTKtitlesList, titlePercent, outputOccupPath, "title");
-			saveAsText(topKstatesList, statePercent, outputStatePath,"state");
-			int t4 = (int) System.currentTimeMillis();
-			System.out.print("Time of saving data: " + (t4 - t3) + "\n");
-			
-			bfreader.close();
-			return true;
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return false;
-	}
-
-	//Use minHeap to select top K
-	public List<Map.Entry<String, Integer>> getTopKFromMap(Map<String, Integer> hashMap, int k) {
-		PriorityQueue<Map.Entry<String, Integer>> topKPq = new PriorityQueue<>(new entryComparator());
+		});	
+		
 		for (Map.Entry<String, Integer> entry : hashMap.entrySet()) {
 			topKPq.offer(entry);
-			if (topKPq.size() > 10) {
+			if (topKPq.size() > k) {
 				topKPq.poll();
 			}
 		}
-		List<Map.Entry<String, Integer>> topKList = new ArrayList<>(15);
+		List<String> topKKeyList = new ArrayList<String>();
 		while (!topKPq.isEmpty()) {
-			topKList.add(topKPq.poll());
+			topKKeyList.add(topKPq.poll().getKey());
 		}
-		return topKList;
+		Collections.reverse(topKKeyList);
+		return topKKeyList;
 	}
 	
-	//unify the format of case entries
-	public String preprocessEntry(String line) {
-		char[] charArr = line.toCharArray();
-		int quoteNum = 0;
-		for (int i = 0; i < charArr.length; i++) {
-			if (charArr[i] == ';' && quoteNum % 2 == 1) {
-				charArr[i] = 'a';
-			}
-			if (charArr[i] == '\"') {
-				quoteNum++;
-			}
-		}
-		String newLine = new String(charArr);
-		return newLine;
-	}
-	
-	public boolean saveAsText(List<Map.Entry<String, Integer>> topKList, List<String> percentList, String filePath, String sortField) {
-		try {
-			//delete old text file first
-			File f1 = new File(filePath);
-			if (f1.isFile()) {
-				f1.delete();
-			}
-			BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
-			if (sortField.contains("title")) {
-				writer.append("TOP_OCCUPATIONS;NUMBER_CERTIFIED_APPLICATIONS;PERCENTAGE");
-			} else if (sortField.contains("state")) {
-				writer.append("TOP_STATES;NUMBER_CERTIFIED_APPLICATIONS;PERCENTAGE");
-			}
-			for (int i = topKList.size() - 1; i > -1; i--) {
-				writer.newLine();
-				writer.append(topKList.get(i).getKey() + ";" + topKList.get(i).getValue() + ";" + percentList.get(i) + "%");
-			}
-			writer.close();
-			return true;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
-	
-	public static class entryComparator implements Comparator<Map.Entry<String, Integer>> {
-		@Override
-		public int compare(Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) {
-			if (e1.getValue() != e2.getValue()) {
-				return e1.getValue() - e2.getValue();
-			} else {
-				return -e1.getKey().compareToIgnoreCase(e2.getKey());
-			}
-		}
-	}
-	
-	public static class StrComparator implements Comparator<String> {
-		@Override
-		public int compare(String s1, String s2) {
-			return s1.compareToIgnoreCase(s2);
-		}
+	//Function 2: Use bucket sorted
+	public List<String> getTopKFromMap(Map<String, Integer> hashMap, int k, int total) {
+		List<String> res = new ArrayList<>();
+		List<String>[] buckets = new List[total + 1];
+        for(String key: hashMap.keySet()) {
+            int freq = hashMap.get(key);
+            if(buckets[freq] == null) {
+                buckets[freq] = new ArrayList<>();
+            }
+            buckets[freq].add(key);
+        }
+        for(int pos = buckets.length - 1; pos >= 0; pos--) {
+            if(buckets[pos] != null) {
+            	//sort buckets first a --> z
+            	buckets[pos].sort((a, b) -> (a.compareToIgnoreCase(b)));
+                for(int i = 0; i < buckets[pos].size() && res.size() < k; i++) {
+                    res.add(buckets[pos].get(i));
+                }
+            }
+        }
+		return res;
 	}
 }
